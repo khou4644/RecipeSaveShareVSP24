c6f0ee27a726ebd129dbfe82703be1a3db2ea066
aptgarcia <adrigarcia@csumb.edu>
2024-04-20 21:16:14 -0700

    Added CreateAccountActivity.java for the button to create account on mainactivity page.

---
+++ app/src/main/java/com/example/recipesavesharevsp24/CreateAccountActivity.java
@@ -0,0 +1,57 @@
+package com.example.privtestproj3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import com.example.privtestproj3.DB.AppDataBase;
+import com.example.privtestproj3.DB.RecipeShareSaveDAO;
+
+public class CreateAccountActivity extends AppCompatActivity {
+        private EditText mUsernameEditText;
+        private EditText mPasswordEditText;
+        private Button mCreateAccountButton;
+
+        private RecipeShareSaveDAO mRecipeShareSaveDAO;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_account_create);
+
+            mUsernameEditText = findViewById(R.id.editTextCreateUserName);
+            mPasswordEditText = findViewById(R.id.editTextCreatePassword);
+            mCreateAccountButton = findViewById(R.id.buttonCreateAccount);
+
+            mRecipeShareSaveDAO = Room.databaseBuilder(this, AppDataBase.class, AppDataBase.DATABASE_NAME)
+                    .allowMainThreadQueries()
+                    .build()
+                    .RecipeShareSaveDAO();
+
+            mCreateAccountButton.setOnClickListener(v -> {
+                String username = mUsernameEditText.getText().toString();
+                String password = mPasswordEditText.getText().toString();
+
+                if (isUsernameDuplicate(username)) {
+                    Toast.makeText(this, "Username already exists", Toast.LENGTH_SHORT).show();
+                } else {
+                    User newUser = new User(username, password, false);
+                    mRecipeShareSaveDAO.insert(newUser);
+                    Toast.makeText(this, "Account created successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent = LoginActivity.intentFactory(CreateAccountActivity.this);
+                    startActivity(intent);
+                    finish();
+                }
+            });
+        }
+
+        private boolean isUsernameDuplicate(String username) {
+            User user = mRecipeShareSaveDAO.getUserByUsername(username);
+            return user != null;
+        }
+    }
