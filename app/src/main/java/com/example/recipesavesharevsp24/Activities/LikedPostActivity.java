c8566e54fafb880c59dcb19830bcc1e4352d86fd
aptgarcia <adrigarcia@csumb.edu>
2024-05-04 19:57:48 -0700

    fixed likedPostActivity to function properly with AdminActivity changes.

--- app/src/main/java/com/example/recipesavesharevsp24/Activities/LikedPostActivity.java
+++ app/src/main/java/com/example/recipesavesharevsp24/Activities/LikedPostActivity.java
@@ -56,6 +56,12 @@ public class LikedPostActivity extends AppCompatActivity {
         mPostAdapter.setPosts(likedPosts);
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
     @SuppressLint("ResourceType")
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
